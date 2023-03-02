b2fc3da5c2dbb2bbffd5b4e76691b195c6edc9f9
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-03-03 00:07:20 +0200

    Notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
-import {SectionStyled} from './Feedback.styled.jsx';
+import Notification from './Notification';
+import { SectionStyled } from './Feedback.styled.jsx';
 import Section from './Section';
 import React, { Component } from 'react';
 
-
-export class App extends Component{
-
+export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
@@ -27,21 +26,30 @@ export class App extends Component{
   countPositiveFeedbackPercentage = () => {
     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
   };
-  
-  render(){
-  return (
-    <SectionStyled>
-    <Section title="Please leave feedback">
-      <FeedbackOptions options={this.state} onLeaveFeedback={this.onLeaveFeedback}/>
-      </Section>
-      <Section>
-{Boolean(this.countTotalFeedback()) &&  <Statistics
-        good={this.state.good}
-        neutral={this.state.neutral}
-        bad={this.state.bad}
-        total={this.countTotalFeedback()}
-        positivePercentage={this.countPositiveFeedbackPercentage()}
-      />}
-    </Section></SectionStyled>
-  )};
-};
+
+  render() {
+    return (
+      <SectionStyled>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section>
+          {Boolean(this.countTotalFeedback()) ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"/>
+          )}
+        </Section>
+      </SectionStyled>
+    );
+  }
+}
